c6baab02923b728eeebffaf97f3897f4ef70abed
h3cth0r <hectorsucre13@gmail.com>
2024-12-02 22:55:44 -0600

    now the tool is generating the extension in the correct place

--- wrapcco/tools.py
+++ wrapcco/tools.py
@@ -11,16 +11,17 @@ def wrapcco_setup(wrappers: List[Wrapper]) -> None:
 # class setuptools.Extension(name: str, sources: list[str | PathLike[str]], *args, py_limited_api: bool = False, **kw)
 
 class Extension(Wrapper):
-    def __init__(self, header_file: str, source_file: str, methods_to_include: List[str], output_name: str, output_path: str = "./", *args, **kwargs):
+    def __init__(self, header_file: str, source_file: str, methods_to_include: List[str], output_name: str, module_name: str="", output_path: str = "./", *args, **kwargs):
         super().__init__(header_file, source_file, methods_to_include, output_name, output_path)
         self.extension = None 
+        self.module_name = module_name
         self.extra_args = args 
         self.extra_kwargs = kwargs 
     def _generate_extension(self):
         self.generate()
 
         self.extension = stExtension(
-            self.output_name, 
+            self.module_name + self.output_name, 
             sources=[
                 os.path.join(self.output_path, self.output_name + ".c"),
                 self.source_file
